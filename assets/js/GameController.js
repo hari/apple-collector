4691e2cd5c591e7bddcec7316b786eaee3097813
Hari Lamichhane <onwithhari@gmail.com>
2017-03-20 17:54:54 +0545

    Add highScore

--- assets/js/GameController.js
+++ assets/js/GameController.js
@@ -1,52 +1,65 @@
- var score = 0,
-     time = 30,
-     timer;
- var IsPlaying = false;
- var timeboard = document.getElementById('time'),
-     scoreboard = document.getElementById('score');
+var score = 0,
+    highScore = 0,
+    time = 30,
+    timer;
+var IsPlaying = false;
+var timeBoard = document.getElementById('time'),
+    scoreBoard = document.getElementById('score'),
+    btnStart = document.getElementById('btn');
 
- function fallDown(apple) {
-     if (!IsPlaying) {
-         return;
-     }
-     apple.setAttribute('data-top', apple.style.top);
-     apple.style.top = "380px";
-     score = score + 5;
-     renderScore();
-     setTimeout(function () {
-         apple.style.display = 'none';
-         restore(apple);
-     }, 501);
- }
+function fallDown(apple) {
+    if (!IsPlaying) {
+        return;
+    }
+    apple.setAttribute('data-top', apple.style.top);
+    apple.style.top = "380px";
+    score = score + 5;
+    renderScore();
+    hideFallenApple(apple);
+}
 
- function restore(apple) {
-     setTimeout(function () {
-         apple.style.top = apple.getAttribute('data-top');
-         apple.style.display = 'inline-block';
-     }, 501);
- }
+function hideFallenApple(apple) {
+    setTimeout(function () {
+        apple.style.display = 'none';
+        restoreFallenApple(apple);
+    }, 501);
+}
 
- function renderScore() {
-     scoreboard.innerText = score;
- }
+function restoreFallenApple(apple) {
+    setTimeout(function () {
+        apple.style.top = apple.getAttribute('data-top');
+        apple.style.display = 'inline-block';
+    }, 501);
+}
 
- function startGame() {
-     IsPlaying = true;
-     timer = setInterval(countDown, 1000);
- }
+function renderScore() {
+    scoreBoard.innerText = score;
+    if (score > highScore) {
+        highScore = score;
+        document.getElementById('high').innerText = highScore;
+    }
+}
 
- function countDown() {
-     time = time - 1;
-     timeboard.innerText = time;
-     if (time == 0) {
-         clearInterval(timer);
-         endGame();
-     }
- }
+function startGame() {
+    btnStart.disabled = "disabled";
+    IsPlaying = true;
+    renderScore();
+    timer = setInterval(countDown, 1000);
+}
 
- function endGame() {
-     IsPlaying = false;
-     alert("Your score is " + score);
-     score = 0;
-     time = 30;
- }
+function countDown() {
+    time = time - 1;
+    timeBoard.innerText = time;
+    if (time == 0) {
+        clearInterval(timer);
+        endGame();
+    }
+}
+
+function endGame() {
+    IsPlaying = false;
+    alert("Your score is " + score);
+    score = 0;
+    time = 30;
+    btnStart.removeAttribute('disabled');
+}
